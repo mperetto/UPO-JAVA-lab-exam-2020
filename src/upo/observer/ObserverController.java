d799f6739b55bf291ce63e9a076cb0b38a95bdfa
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2020-08-19 18:07:01 +0200

    Implementato MouseListener
    Aggiunto evento pressione griglia con recupero delle coordinate della cella.

--- src/upo/observer/ObserverController.java
+++ src/upo/observer/ObserverController.java
@@ -2,8 +2,12 @@ package upo.observer;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class ObserverController implements ActionListener {
+import javax.swing.JLabel;
+
+public class ObserverController implements ActionListener{
 	
 	private GridView v;
 	private ObserverModel m;
@@ -13,12 +17,40 @@ public class ObserverController implements ActionListener {
 		this.m = m;
 		
 		v.btnTest.addActionListener(this);
+		
+		for(int i = 0; i < 10; i++){
+			for(int j = 0; j < 10; j++){
+				v.gridPlayer[i][j].addMouseListener(
+						new MouseAdapter() {
+					        public void mouseClicked(MouseEvent e) {
+					        	
+					        	JLabel l = (JLabel)e.getSource();
+					        	String labelName = l.getName();
+					        	String[] splitted = labelName.split(";");
+					        	Integer r = Integer.parseInt(splitted[0]), c = Integer.parseInt(splitted[1]);
+					        	press(l.getName());
+					        	v.gridPlayer[r][c].removeMouseListener(this);
+					        }
+					    }
+				);
+			}
+		}
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		m.changeGrid();
+	}
+	
+	private void press(String s){
+		String[] splittedString = s.split(";");
+		Integer r = Integer.parseInt(splittedString[0]), c = Integer.parseInt(splittedString[1]);
+		
+		
+		System.out.println("Premuto "+s);
+		System.out.println(r + " " + c);
+		m.press(r, c);
 	}
 
 }
