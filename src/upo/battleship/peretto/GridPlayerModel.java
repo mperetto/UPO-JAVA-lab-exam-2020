0b5c75a6826f26c38d6ee234e6dfaf7f1ae62661
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2020-08-22 12:18:40 +0200

    Revert "Revert "Enum orientamento navi""
    This reverts commit 01ddbea2fb3647dd4025871c8ac72e55a005d1e2.

---
+++ src/upo/battleship/peretto/GridPlayerModel.java
@@ -0,0 +1,23 @@
+package upo.battleship.peretto;
+
+public class GridPlayerModel extends GridModel {
+	
+	/**
+	 * Inizializza una nuova griglia
+	 * */
+	public GridPlayerModel(int rows, int cols) {
+		super(rows, cols);
+	}
+
+	/*@Override
+	public void hitCell(int row, int col) throws IndexOutOfBoundsException {
+		
+	}*/
+	
+	public void changeMessage(String message) 
+    {
+        setChanged();
+        notifyObservers(message);
+    }
+
+}
