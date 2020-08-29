cc3edb0da34a0aa89fc55b600bd24e7a33c3fac9
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2020-08-29 16:30:28 +0200

    Aggiunto Observer
    Aggiunto osservetore su griglia AI.

--- src/upo/battleship/peretto/GridPlayerModel.java
+++ src/upo/battleship/peretto/GridPlayerModel.java
@@ -1,23 +1,34 @@
 package upo.battleship.peretto;
 
-public class GridPlayerModel extends GridModel {
+import java.util.Observable;
+import java.util.Observer;
+
+public class GridPlayerModel extends GridModel implements Observer{
+	
+	private CellStatus[][] enemyGrid;
+	private GridAIModel aiModel;
 	
 	/**
 	 * Inizializza una nuova griglia
 	 * */
-	public GridPlayerModel(int rows, int cols) {
+	public GridPlayerModel(int rows, int cols, GridAIModel aiModel) {
 		super(rows, cols);
+		this.enemyGrid = new CellStatus[rows][cols];
+		this.aiModel = aiModel;
+		this.aiModel.addObserver(this);
 	}
 
-	/*@Override
-	public void hitCell(int row, int col) throws IndexOutOfBoundsException {
+	@Override
+	public void update(Observable o, Object arg) {
 		
-	}*/
-	
-	public void changeMessage(String message) 
-    {
-        setChanged();
-        notifyObservers(message);
-    }
+		CellStatus[][] g = (CellStatus[][])arg;
+		
+		for(int i = 0; i < this.gridRows; i++){
+			for(int j = 0; j < this.gridCols; j++){
+				this.enemyGrid[i][j] = g[i][j];
+			}
+		}
+		
+	}
 
 }
