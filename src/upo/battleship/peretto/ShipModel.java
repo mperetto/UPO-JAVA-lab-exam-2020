53355957ffc81624e3ecf55270b8e6b7a8730186
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2020-08-31 18:40:58 +0200

    Aggiunti commenti
    Aggiunti commenti per documentazione.

--- src/upo/battleship/peretto/ShipModel.java
+++ src/upo/battleship/peretto/ShipModel.java
@@ -6,29 +6,62 @@ public class ShipModel {
 	private int positionRow;
 	private int positionCol;
 	
+	/**
+	 * Crea un nuovo oggetto nave
+	 * 
+	 * @param int headRowCell numero riga cella di partenza
+	 * @param int headColCell numero colonna cella di partenza
+	 * @param ShipType type tipo nave
+	 * @param ShipOrientation orientation orientamento nave
+	 * */
 	public ShipModel(int headRowCell, int headColCell, ShipType type, ShipOrientation orientation) {
 		this.positionRow = headRowCell;
 		this.positionCol = headColCell;
 		this.type = type;
 		this.orientation = orientation;
 	}
-
+	
+	/**
+	 * Restituisce tipo nave
+	 * 
+	 * @return ShipType tipo nave
+	 * */
 	public ShipType getType() {
 		return type;
 	}
 	
+	/**
+	 * Restituisce orientamento nave
+	 * 
+	 * @return ShipOrientation orientamento nave
+	 * */
 	public ShipOrientation getOrientation() {
 		return orientation;
 	}
-
+	
+	/**
+	 * Restituisce riga cella di partenza
+	 * 
+	 * @return int riga cella
+	 * */
 	public int getPositionRow() {
 		return positionRow;
 	}
-
+	
+	/**
+	 * Restituisce colonna cella di partenza
+	 * 
+	 * @return int colonna cella
+	 * */
 	public int getPositionCol() {
 		return positionCol;
 	}
 	
+	/**
+	 * Restituisce dimensione nave
+	 * 
+	 * @return int dimensione della nave
+	 * */
 	public int getDimension() {
 		int dim = 0;
 		
