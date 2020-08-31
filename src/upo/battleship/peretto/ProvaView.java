6a5e08287286af87e92f97488b0b263f814292dc
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2020-08-31 18:39:15 +0200

    Disabilitato codice
    Disabilitato codice temporaneamente.

--- src/upo/battleship/peretto/ProvaView.java
+++ src/upo/battleship/peretto/ProvaView.java
@@ -4,9 +4,9 @@ public class ProvaView {
 
 	public static void main(String[] args) {
 		
-		BattleshipModel m = new BattleshipModel();
+		/*BattleshipModel m = new BattleshipModel();
 		SettingsView v = new SettingsView();
-		BattleshipController c = new BattleshipController(m, v);
+		BattleshipController c = new BattleshipController(m, v);*/
 	}
 
 }
