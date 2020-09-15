ae3a4a68ff3c00e69f1259c0b31f132161694b39
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2020-09-15 15:50:33 +0200

    Aggiunta Classe interna AI
    Aggiunta classe interna AI per gestione colpo AI.

--- src/upo/battleship/peretto/GridAIModel.java
+++ src/upo/battleship/peretto/GridAIModel.java
@@ -1,13 +1,13 @@
 package upo.battleship.peretto;
 
 import java.lang.Math;
+import java.util.Arrays;
 import java.util.Observable;
 import java.util.Observer; 
 
 public class GridAIModel extends GridModel implements Observer {
 	
 	private CellStatus[][] enemyGrid;
-	protected CellStatus[][] enemyGrid;
 	private GridPlayerModel playerModel;
 
 	public GridAIModel(int rows, int cols, GridPlayerModel playerModel) {
@@ -49,7 +49,89 @@ public class GridAIModel extends GridModel implements Observer {
 				this.enemyGrid[i][j] = g[i][j];
 			}
 		}
-		
 	}
 
+}
+
+class AI {
+	
+	CellStatus[][] enemyGrid;
+	private boolean naveIndividuata;
+	private int[] cellaConNavePrecColpita;
+	private int[] cellaConAcquaPrecColpita;
+	private int[] primaCellaColpita;
+	private int[] cellaPrecColpita;
+	private ShipOrientation orientamento;
+	private int estrNaveInd;
+	private boolean affondata;
+	
+	public AI(CellStatus[][] g) {
+		enemyGrid = g;
+		naveIndividuata = false;
+		cellaConNavePrecColpita = new int[2];
+		Arrays.fill(cellaConNavePrecColpita, -1);
+		cellaConAcquaPrecColpita = new int[2];
+		Arrays.fill(cellaConAcquaPrecColpita, -1);
+		primaCellaColpita = new int[2];
+		Arrays.fill(primaCellaColpita, -1);
+		cellaPrecColpita = new int[2];
+		Arrays.fill(cellaPrecColpita, -1);
+		orientamento = null;
+		estrNaveInd = 0;
+		affondata = false;
+	}
+	
+	public int[] colpisci() {
+		
+		if(cellaPrecColpita[0] != -1){
+			
+			if(enemyGrid[cellaPrecColpita[0]][cellaPrecColpita[1]] == CellStatus.CELL_EMPTY_HIT){
+				
+				cellaConAcquaPrecColpita[0] = cellaPrecColpita[0];
+				cellaConAcquaPrecColpita[1] = cellaPrecColpita[1];
+				
+			}
+			else if(enemyGrid[cellaPrecColpita[0]][cellaPrecColpita[1]] == CellStatus.CELL_SHIP_HIT){
+				
+				if(primaCellaColpita[0] == -1){
+					primaCellaColpita[0] = cellaPrecColpita[0];
+					primaCellaColpita[1] = cellaPrecColpita[1];
+					
+					naveIndividuata = true;
+				}
+				
+				cellaConNavePrecColpita[0] = cellaPrecColpita[0];
+				cellaConNavePrecColpita[1] = cellaPrecColpita[1];
+				
+			}
+			
+		}
+		
+		if(!naveIndividuata) {
+			int[] cella = generaCellaCasuale();
+			
+			cellaPrecColpita[0] = cella[0];
+			cellaPrecColpita[1] = cella[1];
+			
+		}
+		else {
+			
+		}
+		
+		return null;
+	}
+	
+	private int[] generaCellaCasuale() {
+		
+		int range = enemyGrid.length; // numeri da 0 a numero di colonne/righe della matrice
+		int[] cell = new int[2];
+		
+		do{
+			cell[0] = (int)Math.random()*range;
+			cell[1] = (int)Math.random()*range;
+		}while(this.enemyGrid[cell[0]][cell[1]] != CellStatus.CELL_EMPTY);		
+		
+		return cell;
+	}
+	
 }
