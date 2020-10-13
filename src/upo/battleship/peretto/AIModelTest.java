0cf9318f16bd4d692d633bc3b6c36aa4d2ca48e5
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2020-10-13 15:38:32 +0200

    Aggiunto test per colpo AI
    codice NON completo

---
+++ src/upo/battleship/peretto/AIModelTest.java
@@ -0,0 +1,37 @@
+package upo.battleship.peretto;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AIModelTest {
+	
+	GridAIModel aiModel;
+	GridPlayerModel playerModel;
+	
+	@Test
+	public void testAI() {
+		playerModel = new GridPlayerModel(10, 10);
+		try{
+			this.playerModel.placeShip(1, 3, ShipOrientation.HORIZONTAL, 5);
+			this.playerModel.placeShip(6, 0, ShipOrientation.VERTICAL, 4);
+			this.playerModel.placeShip(4, 7, ShipOrientation.HORIZONTAL, 3);
+		}
+		catch (IndexOutOfBoundsException e) {
+			fail("Posizionamento navi nella grilgia fallito: "+e.getMessage());
+		}
+		
+		aiModel = new GridAIModel(10, 10, playerModel);
+		System.out.println("1");
+		int[] cella = new int[2];
+		while(!playerModel.isTutteNaviAffondate()){
+			System.out.println("1");
+			cella = aiModel.newMove(1);
+			System.out.println("1");
+			System.out.println("Colpita Cella: ("+cella[0]+","+cella[1]+")");
+			System.out.println("Press Any Key To Continue...");
+
+			new java.util.Scanner(System.in).nextLine();
+		}
+	}
+
+}
