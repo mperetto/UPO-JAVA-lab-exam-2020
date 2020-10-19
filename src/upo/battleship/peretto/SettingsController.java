ccf0dbeb373abf5bdf7d9db8800e1386da84ace8
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2020-10-19 18:54:52 +0200

    Aggiunto recupero dimensione griglia da combobox

--- src/upo/battleship/peretto/SettingsController.java
+++ src/upo/battleship/peretto/SettingsController.java
@@ -1,9 +1,12 @@
 package upo.battleship.peretto;
 
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
 
 public class SettingsController implements ActionListener {
 	
@@ -97,6 +100,24 @@ public class SettingsController implements ActionListener {
 				 * To be implemented
 				 * */
 				System.out.println(((JComponent)e.getSource()).getName());
+				String dimGriglia = String.valueOf(v.cmbDimGriglia.getSelectedItem());
+				
+				int rows, cols;
+				
+				switch(dimGriglia){
+					case "10x10": {
+						rows = 10;
+						cols = 10;
+					} break;
+					case "20x20": {
+						rows = 10;
+						cols = 10;
+					} break;
+					default: {
+						rows = 10;
+						cols = 10;
+					}
+				}
 			} break;
 			
 		}
